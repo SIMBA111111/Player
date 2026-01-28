c3d23da3c4a623564a0db7d684de47c56f0e9f0a
Vlad Rudnev <rudnev@zeroagency.ru>
2026-01-28 14:03:34 +0300

    fix

--- src/entites/play-buttons/ui/play-buttons.tsx
+++ src/entites/play-buttons/ui/play-buttons.tsx
@@ -6,7 +6,7 @@ import { handleRewind, handleForward } from "../lib/handlers"
 
 import { usePlayerContext } from "../../../component"
 import rewindIcon from '../../../assets/images/png/rewind10_Bold.png'
-import frowardIcon from '../../../assets/images/png/forward10_bold.png'
+import frowardIcon from '../../../assets/images/png/forward10_Bold.png'
 import playIcon from '../../../assets/images/png/play-btn.png'
 import stopIcon from '../../../assets/images/png/stop-btn.png'
 
